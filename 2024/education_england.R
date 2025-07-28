80dbc7f2dbc2ef245a2ae9eb3cf3062447f6b757
mhkirmizi <mhkirmizi@gmail.com>
2025-07-28 15:06:56 +0300

    A small fix

--- 2024/education_england.R
+++ 2024/education_england.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(ggstatsplot)
 library(showtext)
 library(ggrain)
 ## Fonts
@@ -12,16 +11,15 @@ subtitle <- "The presence of a university in a town does not influence the avera
 caption <- "Data:  The UK Office for National Statistics | Vis: MhKirmizi"
 ### Data 
 english_education <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-01-23/english_education.csv')
-english_education |>
-  filter(!is.na(university_flag)) |> 
-  ggbetweenstats(
-    x = university_flag, 
-    y = education_score
-  ) 
+
 english_education |>
   filter(!is.na(university_flag)) |> 
   ggplot(aes(x = university_flag, y = education_score, fill = university_flag)) +
-  geom_rain(alpha = .8) +
+  geom_rain(alpha = .8,
+            boxplot.args = list(
+              color = "black",
+              outlier.shape = NA), 
+            point.args = list(alpha = .4)) +
   labs(
     x = "",
     y = "Education score", 
@@ -32,12 +30,13 @@ english_education |>
     ) +
   theme_classic(base_family = 'Outfit') +
   theme(
-    plot.title = element_text(size = 32, hjust = .5, face= "bold"), 
-    plot.subtitle = element_text(size = 18, hjust = .5),
+    plot.title = element_text(size = 42, hjust = .5, face= "bold"), 
+    plot.subtitle = element_text(size = 24, hjust = .5),
     plot.caption = element_text(size = 10),
     axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
     axis.text.x = element_blank(),
-    legend.position = "bottom"
+    legend.position = "top"
   )
 ggsave("education_england.png", width = 1920, height = 1080, units = "px", dpi = 132)
 
